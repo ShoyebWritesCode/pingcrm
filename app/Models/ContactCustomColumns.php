278dcc795d3d8354e733e2f40fac555c9dc0bf0c
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2024-08-21 18:26:16 +0600

    Add Remove Additional Data Upon Delition

--- app/Models/ContactCustomColumns.php
+++ app/Models/ContactCustomColumns.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class ContactCustomColumns extends Model
 {
@@ -16,5 +17,41 @@ class ContactCustomColumns extends Model
     public function ContactsCustomData(): HasMany
     {
         return $this->hasMany(ContactsCustomData::class);
+    }
+
+
+    /**
+     * The "booted" method of the model.
+     * Here we can define events listener on model changes
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::deleted(function ($model) {
+            $model->handleDeletion();
+        });
+    }
+
+    /**
+     * Handle the deletion of the model.
+     *
+     * @return void
+     */
+    protected function handleDeletion()
+    {
+        $columnName = $this->name;
+        $contacts = DB::table('contacts')->whereNotNull('additional_data')->get();
+        foreach ($contacts as $contact) {
+            $additionalData = json_decode($contact->additional_data, true);
+
+            if (isset($additionalData[$columnName])) {
+                unset($additionalData[$columnName]);
+
+                DB::table('contacts')->where('id', $contact->id)->update([
+                    'additional_data' => json_encode($additionalData),
+                ]);
+            }
+        }
     }
 }
