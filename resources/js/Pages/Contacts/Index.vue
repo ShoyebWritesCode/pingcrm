9812bae6a7a30a7cf1cdfa8ae2ca99c12e5d3af9
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2024-08-22 18:08:40 +0600

    Add Confirmation Pop Up

--- resources/js/Pages/Contacts/Index.vue
+++ resources/js/Pages/Contacts/Index.vue
@@ -13,10 +13,47 @@
         </select>
       </search-filter>
       <div>
-        <button v-if="deleteButtonVisibility" @click="deleteSelected" class="btn-red mx-4 px-3 py-2"
-          title="Add New Columns">
+        <button v-if="deleteButtonVisibility" @click="showConfirmation = true" class="btn-red mx-4 px-3 py-2">
           Delete Selected
         </button>
+
+        <div v-if="showConfirmation" tabindex="-1"
+          class="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50"
+          @click.self="hideConfirmation">
+
+          <div class="relative p-4 w-full max-w-md max-h-full">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <button type="button"
+                class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                @click="hideConfirmation">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                  viewBox="0 0 14 14">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+                <span class="sr-only">Close modal</span>
+              </button>
+              <div class="p-4 md:p-5 text-center">
+                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete
+                  these contacts?</h3>
+                <button @click="deleteSelected" type="button"
+                  class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                  Yes, I'm sure
+                </button>
+                <button @click="hideConfirmation" type="button"
+                  class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                  No, cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <button @click="showModal = true" class="btn-indigo mx-4 px-3 py-2" title="Add New Columns">
           <font-awesome-icon icon="table-cells" />
         </button>
@@ -284,6 +321,7 @@ export default {
       showCsvModal: false,
       PreviewModal: false,
       editColumnModal: false,
+      showConfirmation: false,
       // deleteButtonVisibility: false,
       csvData: [],
       csvColumns: [],
@@ -317,22 +355,20 @@ export default {
   methods: {
 
     deleteSelected() {
+      this.showConfirmation = false;
       const selectedIds = this.selectedContacts
         .filter(contact => contact.selected)
         .map(contact => contact.id);
 
       if (selectedIds.length > 0) {
-        if (confirm('Are you sure you want to delete these contacts?')) {
-          this.$inertia.delete(`/contacts/delete/${selectedIds}`, {
-            preserveScroll: true,
-            onSuccess: () => {
-              window.location.reload();
+        this.$inertia.delete(`/contacts/delete/${selectedIds}`, {
+          preserveScroll: true,
+          onSuccess: () => {
+            window.location.reload();
 
-            },
-          });
+          },
+        });
 
-
-        }
       } else {
         alert('No contacts selected.');
       }
@@ -470,7 +506,10 @@ export default {
           console.error("Error occurred while deleting column:", error);
         }
       });
-    }
+    },
+    hideConfirmation() {
+      this.showConfirmation = false;
+    },
 
   },
 }
